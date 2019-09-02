060767bde1134e31ddedb457d3c02bb343229d17
LeonChisum <leonchisum3306@gmail.com>
2019-09-01 21:29:55 -0400

    MVP complete

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -2,33 +2,25 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import styled from "styled-components";
 import Axios from 'axios';
+import Card from './components/Cards'
+
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
-  const [name, setName] = useState('')
-  const [homeworld, setHomeworld] = useState('')
-  const [birthdate, setBirthdate] = useState('')
-  const [films, setFilms] = useState('')
-  const [species, setSpecies] = useState('')
+  const [data, setData] = useState([{}]);
+  const [page, setPage] = useState(1);
   
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
   useEffect(() => {
-      Axios.get(`https://swapi.co/api/people/?page=1`)
+      Axios.get(`https://swapi.co/api/people/?page=${page}`)
         .then(res => {
           console.log(res);
-          let results = res.data.results
-          results.forEach(person => {
-            setName(person.name)
-            setHomeworld(person.homeworld)
-            setBirthdate(person.birth_year)
-            setFilms(person.films)
-            setSpecies(person.species)
-          }
-        )})
-  },)
+          setData(res.data.results)
+       })
+  },[page])
 
   const Div = styled.div`
         width: 85%;
@@ -40,9 +32,15 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
-      <Div>
-        <h1> { name } </h1>
-      </Div>
+      {data.map(person => {
+        return <Card name={person.name} 
+                    species={person.species} 
+                    homeworld={person.homeworld}
+                    birthdate={person.birth_year}
+                    films={person.films}
+                    url={person.url}/>
+                
+      })}
     </div>
   );
 }
--- starwars/src/components/Cards.js
+++ starwars/src/components/Cards.js
@@ -1,12 +1,24 @@
 import React from "react"
-import Styled from "styled-components"
-const Card = () =>{
+import styled from "styled-components"
+const Card = (props) =>{
 
-    const div = styled.div`
-    
+    const Div = styled.div`
+        width: 85%;
+        margin: 0 auto;
+        border-top: 2px solid rgba(255,255,255,.2);
+        border-bottom: 2px solid rgba(255,255,255,.2);
+        
     `
     return(
-
+        <Div key={props.url}>
+        <h1>{ props.name }</h1>
+        <ul>
+            <li>Species: { props.species }</li>
+            <li>Planet World: { props.homeworld }</li>
+            <li>Year of Origin: { props.birthdate } </li>
+            <li>Appearances: { props.films } </li> 
+        </ul>
+        </Div>
     );
 };
 
